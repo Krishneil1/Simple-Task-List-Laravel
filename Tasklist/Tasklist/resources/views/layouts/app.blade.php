309e06bf4f104283429a385b03c9025c6f61b7df
Krishneel <Krishneil1@gmail.com>
2017-03-05 19:03:47 +1300

    Complete Task List

---
+++ Tasklist/Tasklist/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> Tasks</title>
+
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- This Bootstrap -->
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+      <div class = "container">
+        @yield('content')
+      </div>
+    </body>
+</html>
